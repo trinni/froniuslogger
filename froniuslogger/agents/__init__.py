bed7bca11c46ebd30984999098a7c92b75b99f5a
trinni <mail@duytrih>
2017-10-02 23:41:21 +0200

    added parameter-validation

--- froniuslogger/agents/__init__.py
+++ froniuslogger/agents/__init__.py
@@ -11,10 +11,13 @@ class BaseAgent(object):
         self.info_url = urlparse.urljoin(host_address, "solar_api/GetAPIVersion.cgi")
         self.info_response = requests.get(self.info_url)
         self.info = None
+        self._parameters = None
+
         # Default values according to Fronius Solar API v1, 2.3.1, Listing 3
         self.api_version = 0
         self.base_url = '/solar_api/'
 
+        # Parse api version from info response
         if self.info_response.status_code != 404:
             self.info = self.info_response.json()
             if u'APIVersion' in self.info:
@@ -25,18 +28,36 @@ class BaseAgent(object):
         self.api_url = urlparse.urljoin(self.api_path, self.endpoint_name)
 
     @property
-    def endpoint_name(self):
-        return 'GetAPIVersion.cgi'
+    def valid_parameter_values(self):
+        # e.g.
+        # return {}
+        raise NotImplementedError
 
-    def get_response(self, parameters=None):
-        request_url = self._make_request_url(parameters)
+    @property
+    def endpoint_name(self):
+        # e.g.
+        # return 'GetAPIVersion.cgi'
+        raise NotImplementedError
+
+    def get_response(self):
+        request_url = self._make_request_url(self._parameters)
         response = requests.get(self.api_url)
         assert response.status_code == 200
         return response.json()
 
+    def set_parameters(self, parameters):
+        self._validate_parameters(parameters)
+        self._parameters = parameters
+
+    def _validate_parameters(self, parameters):
+        assert isinstance(parameters, types.ListType)
+        for key, val in parameters:
+            assert key in self.valid_parameter_values
+            assert val in self.valid_parameter_values[key]
+
     def _make_request_url(self, parameters=None):
         request_url = self.api_url
         if parameters and isinstance(parameters, types.DictType):
-            request_url = request_url+'?'
+            request_url = request_url + '?'
             request_url = urllib.urlencode(parameters)
         return request_url
